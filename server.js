fd020a3faf7b95a1cb86cd7d7db8b99928bd27b3
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-02-13 16:27:31 +0000

    Updated cors

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const cors = require("cors");
 const db = require("./db");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(cors());
 app.use(morgan("dev"));
